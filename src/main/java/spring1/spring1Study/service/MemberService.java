ce8dfde29330a189eedd69195f64462ed988478c
최영찬 <gks54410@gmail.com>
2022-02-09 22:35:05 +0900

    4. 회원 서비스 개발

---
+++ src/main/java/spring1/spring1Study/service/MemberService.java
@@ -0,0 +1,38 @@
+package spring1.spring1Study.service;
+
+import spring1.spring1Study.domain.Member;
+import spring1.spring1Study.repository.MemberRepository;
+import spring1.spring1Study.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    /*  회원가입  */
+    public Long join(Member member){
+        // 같은 이름 회원 X
+        validateDuplicateMember(member);
+
+        memberRepository.save(member);
+
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+            .ifPresent(m -> {
+                throw new IllegalStateException("이미 존재하는 회원입니다.");
+            });
+    }
+
+    /*  전체 회원 조회  */
+    private List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId){
+        return memberRepository.findById(memberId);
+    }
+}

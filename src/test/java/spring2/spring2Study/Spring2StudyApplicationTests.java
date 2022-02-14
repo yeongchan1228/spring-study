f92771de5d83acf00eac42f593fa96171ebdc261
최영찬 <gks54410@gmail.com>
2022-02-14 15:12:09 +0900

    1. 프로젝트 기본 설정

---
+++ src/test/java/spring2/spring2Study/Spring2StudyApplicationTests.java
@@ -0,0 +1,13 @@
+package spring2.spring2Study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring2StudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

859932595a13eadf43eb1c6341668010d47aeda6
최영찬 <gks54410@gmail.com>
2022-05-22 23:42:37 +0900

    Initial Commit

---
+++ spring3-study/src/main/java/spring3/spring3study/Spring3StudyApplication.java
@@ -0,0 +1,13 @@
+package spring3.spring3study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring3StudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring3StudyApplication.class, args);
+	}
+
+}
---
+++ spring3-study/src/test/java/spring3/spring3study/Spring3StudyApplicationTests.java
@@ -0,0 +1,13 @@
+package spring3.spring3study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring3StudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

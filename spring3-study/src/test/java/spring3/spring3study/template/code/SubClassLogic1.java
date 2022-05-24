63224e34feaa7663d6ef6fbea00766b9d3fb0b73
최영찬 <gks54410@gmail.com>
2022-05-24 23:43:56 +0900

    Feat: Template Method Pattern 추상화 클래스의 자식 클래스 생성

---
+++ spring3-study/src/test/java/spring3/spring3study/template/code/SubClassLogic1.java
@@ -0,0 +1,12 @@
+package spring3.spring3study.template.code;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class SubClassLogic1 extends AbstractTemplate{
+
+    @Override
+    protected void call() {
+        log.info("비즈니스 로직1 실행");
+    }
+}

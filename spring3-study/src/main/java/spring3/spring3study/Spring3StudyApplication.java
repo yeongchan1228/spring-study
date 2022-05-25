b983827d2a3192f55d882af1e838d7cdc515a9fb
최영찬 <gks54410@gmail.com>
2022-05-25 23:44:09 +0900

    Feat: Component Scan 패키지 지정 및 Configuration Class Import

--- spring3-study/src/main/java/spring3/spring3study/Spring3StudyApplication.java
+++ spring3-study/src/main/java/spring3/spring3study/Spring3StudyApplication.java
@@ -2,8 +2,11 @@ package spring3.spring3study;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+import spring3.spring3study.config.proxy.AppV1Config;
 
-@SpringBootApplication
+@Import(AppV1Config.class)
+@SpringBootApplication(scanBasePackages = "spring3.spring3study.app.proxy")
 public class Spring3StudyApplication {
 
 	public static void main(String[] args) {

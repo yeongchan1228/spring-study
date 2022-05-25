311b838ef0a9e18c6bc18823c06572f3ce09104a
최영찬 <gks54410@gmail.com>
2022-05-25 17:28:46 +0900

    Feat: TraceTemplate Bean 등록

--- spring3-study/src/main/java/spring3/spring3study/config/LogTraceConfig.java
+++ spring3-study/src/main/java/spring3/spring3study/config/LogTraceConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import spring3.spring3study.trace.logtrace.LogTrace;
 import spring3.spring3study.trace.logtrace.ThreadLocalLogTrace;
+import spring3.spring3study.trace.template.callback.TraceTemplate;
 
 @Configuration
 public class LogTraceConfig {
@@ -11,5 +12,10 @@ public class LogTraceConfig {
     @Bean
     public LogTrace createLogTrace(){
         return new ThreadLocalLogTrace();
+    }
+
+    @Bean
+    public TraceTemplate createLogTemplate(){
+        return new TraceTemplate(createLogTrace());
     }
 }

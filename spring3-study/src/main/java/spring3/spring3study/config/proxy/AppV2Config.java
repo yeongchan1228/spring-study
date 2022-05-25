b33d74e5a6680852ed89136235168deb5b296e1f
최영찬 <gks54410@gmail.com>
2022-05-25 23:52:48 +0900

    Feat: Proxy V2 기능 수동 빈 등록

---
+++ spring3-study/src/main/java/spring3/spring3study/config/proxy/AppV2Config.java
@@ -0,0 +1,25 @@
+package spring3.spring3study.config.proxy;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import spring3.spring3study.app.proxy.v1.*;
+import spring3.spring3study.app.proxy.v2.OrderControllerV2;
+import spring3.spring3study.app.proxy.v2.OrderRepositoryV2;
+import spring3.spring3study.app.proxy.v2.OrderServiceV2;
+
+@Configuration
+public class AppV2Config {
+
+    @Bean
+    public OrderControllerV2 createOrderControllerV2(){return new OrderControllerV2(createOrderServiceV2());}
+
+    @Bean
+    public OrderServiceV2 createOrderServiceV2() {
+        return new OrderServiceV2(createOrderRepository());
+    }
+
+    @Bean
+    public OrderRepositoryV2 createOrderRepository() {
+        return new OrderRepositoryV2();
+    }
+}

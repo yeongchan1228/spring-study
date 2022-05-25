ef7d26fc3e38426178c0d1bd25a59be2b456be3c
최영찬 <gks54410@gmail.com>
2022-05-25 23:43:27 +0900

    Feat: Proxy V1 기능 수동 빈 등록

---
+++ spring3-study/src/main/java/spring3/spring3study/config/proxy/AppV1Config.java
@@ -0,0 +1,23 @@
+package spring3.spring3study.config.proxy;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import spring3.spring3study.app.proxy.v1.*;
+
+@Configuration
+public class AppV1Config {
+
+    @Bean
+    public OrderControllerV1 createOrderControllerV1(){
+        return new OrderControllerImpl(createOrderServiceV1());
+    }
+
+    @Bean
+    public OrderServiceV1 createOrderServiceV1() {
+        return new OrderServiceV1Impl(createOrderRepository());
+    }
+
+    public OrderRepositoryV1 createOrderRepository() {
+        return new OrderRepositoryV1Impl();
+    }
+}

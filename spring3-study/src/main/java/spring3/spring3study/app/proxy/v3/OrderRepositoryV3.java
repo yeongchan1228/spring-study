89241fb5409f072ee26339fe6f3f8be766797841
최영찬 <gks54410@gmail.com>
2022-05-26 09:42:38 +0900

    Feat: Bean 자동 등록 itemId 저장 기능 구현

---
+++ spring3-study/src/main/java/spring3/spring3study/app/proxy/v3/OrderRepositoryV3.java
@@ -0,0 +1,20 @@
+package spring3.spring3study.app.proxy.v3;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class OrderRepositoryV3 {
+
+    public void save(String itemId){
+        if(itemId.equals("ex")) throw new IllegalStateException("예외 발생!");
+        sleep(1000);
+    }
+
+    private void sleep(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}

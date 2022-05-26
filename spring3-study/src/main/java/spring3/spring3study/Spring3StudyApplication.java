d604be83167b74c1b723919744481152f1d01177
최영찬 <gks54410@gmail.com>
2022-05-26 09:42:58 +0900

    Feat: AppV2Config 설정 정보 등록 추가

--- spring3-study/src/main/java/spring3/spring3study/Spring3StudyApplication.java
+++ spring3-study/src/main/java/spring3/spring3study/Spring3StudyApplication.java
@@ -4,8 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 import spring3.spring3study.config.proxy.AppV1Config;
+import spring3.spring3study.config.proxy.AppV2Config;
 
-@Import(AppV1Config.class)
+@Import({AppV1Config.class, AppV2Config.class})
 @SpringBootApplication(scanBasePackages = "spring3.spring3study.app.proxy")
 public class Spring3StudyApplication {
 

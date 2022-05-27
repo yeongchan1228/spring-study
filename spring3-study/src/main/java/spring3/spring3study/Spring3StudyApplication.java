f43037186500c44a340a409d1643ab81bd08ce9e
최영찬 <gks54410@gmail.com>
2022-05-28 00:03:15 +0900

    Feat: InterfaceProxyConfig 등록

--- spring3-study/src/main/java/spring3/spring3study/Spring3StudyApplication.java
+++ spring3-study/src/main/java/spring3/spring3study/Spring3StudyApplication.java
@@ -2,11 +2,16 @@ package spring3.spring3study;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import spring3.spring3study.config.proxy.AppV1Config;
 import spring3.spring3study.config.proxy.AppV2Config;
+import spring3.spring3study.config.proxy.v1_proxy.interface_proxy.InterfaceProxyConfig;
+import spring3.spring3study.trace.logtrace.LogTrace;
+import spring3.spring3study.trace.logtrace.ThreadLocalLogTrace;
 
-@Import({AppV1Config.class, AppV2Config.class})
+//@Import({AppV1Config.class, AppV2Config.class})
+@Import({InterfaceProxyConfig.class})
 @SpringBootApplication(scanBasePackages = "spring3.spring3study.app.proxy")
 public class Spring3StudyApplication {
 
@@ -14,4 +19,9 @@ public class Spring3StudyApplication {
 		SpringApplication.run(Spring3StudyApplication.class, args);
 	}
 
+
+	@Bean
+	public LogTrace logTrace(){
+		return new ThreadLocalLogTrace();
+	}
 }

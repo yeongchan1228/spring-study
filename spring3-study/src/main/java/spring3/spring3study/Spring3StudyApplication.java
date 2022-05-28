a754d1ff1981d57d867bba8412a3219ba2001559
최영찬 <gks54410@gmail.com>
2022-05-29 00:21:29 +0900

    Feat: ConcreteProxyConfig 등록

--- spring3-study/src/main/java/spring3/spring3study/Spring3StudyApplication.java
+++ spring3-study/src/main/java/spring3/spring3study/Spring3StudyApplication.java
@@ -6,12 +6,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import spring3.spring3study.config.proxy.AppV1Config;
 import spring3.spring3study.config.proxy.AppV2Config;
+import spring3.spring3study.config.proxy.v1_proxy.concrete_proxy.ConcreteProxyConfig;
 import spring3.spring3study.config.proxy.v1_proxy.interface_proxy.InterfaceProxyConfig;
 import spring3.spring3study.trace.logtrace.LogTrace;
 import spring3.spring3study.trace.logtrace.ThreadLocalLogTrace;
 
 //@Import({AppV1Config.class, AppV2Config.class})
-@Import({InterfaceProxyConfig.class})
+//@Import({InterfaceProxyConfig.class})
+@Import({ConcreteProxyConfig.class})
 @SpringBootApplication(scanBasePackages = "spring3.spring3study.app.proxy")
 public class Spring3StudyApplication {
 

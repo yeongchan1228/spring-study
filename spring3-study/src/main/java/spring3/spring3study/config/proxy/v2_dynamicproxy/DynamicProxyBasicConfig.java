acfecd47e30b35123e59f026ac5a6e364f7c05e7
최영찬 <gks54410@gmail.com>
2022-05-30 11:29:06 +0900

    Feat: JdkDynamicProxy LogTrace 설정 정보 추가

---
+++ spring3-study/src/main/java/spring3/spring3study/config/proxy/v2_dynamicproxy/DynamicProxyBasicConfig.java
@@ -0,0 +1,34 @@
+package spring3.spring3study.config.proxy.v2_dynamicproxy;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import spring3.spring3study.app.proxy.v1.*;
+import spring3.spring3study.config.proxy.v2_dynamicproxy.handler.LogTraceBasicHandler;
+import spring3.spring3study.trace.logtrace.LogTrace;
+
+import java.lang.reflect.Proxy;
+
+@Configuration
+public class DynamicProxyBasicConfig {
+
+    @Bean
+    public OrderControllerV1 orderControllerV1(LogTrace logTrace){
+        OrderControllerV1 orderController = new OrderControllerImpl(orderServiceV1(logTrace));
+        return (OrderControllerV1) Proxy.newProxyInstance(OrderControllerV1.class.getClassLoader(),
+                new Class[]{OrderControllerV1.class}, new LogTraceBasicHandler(orderController, logTrace));
+    }
+
+    @Bean
+    public OrderServiceV1 orderServiceV1(LogTrace logTrace){
+        OrderServiceV1 orderService = new OrderServiceV1Impl(orderRepositoryV1(logTrace));
+        return (OrderServiceV1) Proxy.newProxyInstance(OrderServiceV1.class.getClassLoader(),
+                new Class[]{OrderServiceV1.class}, new LogTraceBasicHandler(orderService, logTrace));
+    }
+
+    @Bean
+    public OrderRepositoryV1 orderRepositoryV1(LogTrace logTrace){
+        OrderRepositoryV1 orderRepository = new OrderRepositoryV1Impl();
+        return (OrderRepositoryV1) Proxy.newProxyInstance(OrderRepositoryV1.class.getClassLoader(),
+                new Class[]{OrderRepositoryV1.class}, new LogTraceBasicHandler(orderRepository, logTrace));
+    }
+}

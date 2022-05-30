edcd00ae01e9a6b1858cc2a8df722414702b557a
최영찬 <gks54410@gmail.com>
2022-05-30 10:53:07 +0900

    Feat: JdkDynamicProxy Handler 구현

---
+++ spring3-study/src/test/java/spring3/spring3study/proxy/jdkdynamic/code/TimeInvocationHandler.java
@@ -0,0 +1,26 @@
+package spring3.spring3study.proxy.jdkdynamic.code;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+
+@Slf4j
+public class TimeInvocationHandler implements InvocationHandler {
+
+    private final Object target;
+
+    public TimeInvocationHandler(Object target) {
+        this.target = target;
+    }
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        log.info("TimeProxy 실행");
+        long startTime = System.currentTimeMillis();
+        Object result = method.invoke(target, args);
+        long endTime = System.currentTimeMillis();
+        log.info("TimeProxy 종료 - 실행 시간 = {}", endTime - startTime);
+        return result;
+    }
+}

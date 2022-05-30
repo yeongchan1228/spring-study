1403e9170e23d1e9b93e3f4a593bebb074fa4a01
최영찬 <gks54410@gmail.com>
2022-05-30 11:29:55 +0900

    Feat: DynamicProxyFilterConfig 설정 정보 적용

--- spring3-study/src/main/java/spring3/spring3study/Spring3StudyApplication.java
+++ spring3-study/src/main/java/spring3/spring3study/Spring3StudyApplication.java
@@ -8,12 +8,16 @@ import spring3.spring3study.config.proxy.AppV1Config;
 import spring3.spring3study.config.proxy.AppV2Config;
 import spring3.spring3study.config.proxy.v1_proxy.concrete_proxy.ConcreteProxyConfig;
 import spring3.spring3study.config.proxy.v1_proxy.interface_proxy.InterfaceProxyConfig;
+import spring3.spring3study.config.proxy.v2_dynamicproxy.DynamicProxyBasicConfig;
+import spring3.spring3study.config.proxy.v2_dynamicproxy.DynamicProxyFilterConfig;
 import spring3.spring3study.trace.logtrace.LogTrace;
 import spring3.spring3study.trace.logtrace.ThreadLocalLogTrace;
 
 //@Import({AppV1Config.class, AppV2Config.class})
 //@Import({InterfaceProxyConfig.class})
-@Import({ConcreteProxyConfig.class})
+//@Import({ConcreteProxyConfig.class})
+//@Import(value = {DynamicProxyBasicConfig.class})
+@Import(value = {DynamicProxyFilterConfig.class})
 @SpringBootApplication(scanBasePackages = "spring3.spring3study.app.proxy")
 public class Spring3StudyApplication {
 

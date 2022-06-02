733e88298b9cd9fbd26d626ceb5387f166a450ce
최영찬 <gks54410@gmail.com>
2022-06-02 14:53:54 +0900

    Test: 다중 Proxy 생성을 통한 다중 Advisor 적용

---
+++ spring3-study/src/test/java/spring3/spring3study/proxy/advisor/MultiAdvisorProxy.java
@@ -0,0 +1,60 @@
+package spring3.spring3study.proxy.advisor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.Pointcut;
+import org.springframework.aop.framework.ProxyFactory;
+import org.springframework.aop.support.DefaultPointcutAdvisor;
+import org.springframework.aop.support.NameMatchMethodPointcut;
+import spring3.spring3study.proxy.common.advice.TimeAdvice;
+import spring3.spring3study.proxy.common.service.ServiceImpl;
+import spring3.spring3study.proxy.common.service.ServiceInterface;
+
+@Slf4j
+public class MultiAdvisorProxy {
+
+    /**
+     * 프록시를 여러 개 생성해야 한다는 단점
+     */
+    @Test
+    @DisplayName("여러 Proxy 생성")
+    public void multiAdvisorTest1() throws Exception {
+        // given
+        ServiceInterface target = new ServiceImpl();
+        ProxyFactory proxyFactory = new ProxyFactory(target);
+        DefaultPointcutAdvisor advisor1 = new DefaultPointcutAdvisor(Pointcut.TRUE, new Advice1());
+        proxyFactory.addAdvisor(advisor1);
+        ServiceInterface proxy1 = (ServiceInterface) proxyFactory.getProxy();
+
+        proxyFactory = new ProxyFactory(proxy1);
+        DefaultPointcutAdvisor advisor2 = new DefaultPointcutAdvisor(Pointcut.TRUE, new Advice2());
+        proxyFactory.addAdvisor(advisor2);
+        ServiceInterface proxy2 = (ServiceInterface) proxyFactory.getProxy();
+
+        proxy2.save();
+        proxy2.find();
+    }
+
+    @Slf4j
+    static class Advice1 implements MethodInterceptor{
+
+        @Override
+        public Object invoke(MethodInvocation invocation) throws Throwable {
+            log.info("advice1 호출");
+            return invocation.proceed();
+        }
+    }
+
+    @Slf4j
+    static class Advice2 implements MethodInterceptor{
+
+        @Override
+        public Object invoke(MethodInvocation invocation) throws Throwable {
+            log.info("advice2 호출");
+            return invocation.proceed();
+        }
+    }
+}

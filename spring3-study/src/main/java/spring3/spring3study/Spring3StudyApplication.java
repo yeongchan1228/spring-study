e60e177ef3565e5d4235efa7503e2deadc8a6f7d
최영찬 <gks54410@gmail.com>
2022-06-02 15:27:42 +0900

    Feat: ProxyFactoryConfigV2 설정 정보 적용 추가

--- spring3-study/src/main/java/spring3/spring3study/Spring3StudyApplication.java
+++ spring3-study/src/main/java/spring3/spring3study/Spring3StudyApplication.java
@@ -11,6 +11,7 @@ import spring3.spring3study.config.proxy.v1_proxy.interface_proxy.InterfaceProxy
 import spring3.spring3study.config.proxy.v2_dynamicproxy.DynamicProxyBasicConfig;
 import spring3.spring3study.config.proxy.v2_dynamicproxy.DynamicProxyFilterConfig;
 import spring3.spring3study.config.proxy.v3_proxyfactory.ProxyFactoryConfigV1;
+import spring3.spring3study.config.proxy.v3_proxyfactory.ProxyFactoryConfigV2;
 import spring3.spring3study.trace.logtrace.LogTrace;
 import spring3.spring3study.trace.logtrace.ThreadLocalLogTrace;
 
@@ -19,7 +20,7 @@ import spring3.spring3study.trace.logtrace.ThreadLocalLogTrace;
 //@Import({ConcreteProxyConfig.class})
 //@Import(value = {DynamicProxyBasicConfig.class})
 //@Import(value = {DynamicProxyFilterConfig.class})
-@Import(value = {ProxyFactoryConfigV1.class})
+@Import(value = {ProxyFactoryConfigV1.class, ProxyFactoryConfigV2.class})
 @SpringBootApplication(scanBasePackages = "spring3.spring3study.app.proxy")
 public class Spring3StudyApplication {
 

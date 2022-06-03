ed29aaf8083290c96173572e02c64db072ab4679
최영찬 <gks54410@gmail.com>
2022-06-03 22:21:15 +0900

    Initial Commit

---
+++ spring-aop-study/src/test/java/springstudy/springaopstudy/SpringAopStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package springstudy.springaopstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAopStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

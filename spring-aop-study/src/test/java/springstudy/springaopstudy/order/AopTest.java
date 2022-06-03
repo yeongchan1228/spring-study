d8c06d359d987dd294c70c9d4132abff362c682d
최영찬 <gks54410@gmail.com>
2022-06-03 22:40:56 +0900

    Test: 기본 예제 테스트

---
+++ spring-aop-study/src/test/java/springstudy/springaopstudy/order/AopTest.java
@@ -0,0 +1,33 @@
+package springstudy.springaopstudy.order;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.support.AopUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.*;
+
+@Slf4j
+@SpringBootTest
+public class AopTest {
+    
+    @Autowired OrderRepository orderRepository;
+    @Autowired OrderService orderService;
+
+    @Test
+    public void aopInfo() throws Exception {
+        log.info("isAopProxy, orderService = {}", AopUtils.isAopProxy(orderService));
+        log.info("isAopProxy, orderRepository = {}", AopUtils.isAopProxy(orderRepository));
+    }
+
+    @Test
+    public void success() throws Exception {
+        orderService.orderItem("itemA");
+    }
+
+    @Test
+    public void exception() throws Exception {
+        assertThatThrownBy(() -> orderService.orderItem("ex")).isInstanceOf(IllegalStateException.class);
+    }
+}

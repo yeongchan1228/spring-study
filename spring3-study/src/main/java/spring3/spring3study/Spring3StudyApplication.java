db6dddea52dd5b928ddb2fb089e330f1c1316b73
최영찬 <gks54410@gmail.com>
2022-06-03 19:59:38 +0900

    Feat: AopConfig 등록

--- spring3-study/src/main/java/spring3/spring3study/Spring3StudyApplication.java
+++ spring3-study/src/main/java/spring3/spring3study/Spring3StudyApplication.java
@@ -14,6 +14,7 @@ import spring3.spring3study.config.proxy.v3_proxyfactory.ProxyFactoryConfigV1;
 import spring3.spring3study.config.proxy.v3_proxyfactory.ProxyFactoryConfigV2;
 import spring3.spring3study.config.proxy.v4_postprocessor.BeanPostProcessorConfig;
 import spring3.spring3study.config.proxy.v5_autoproxy.AutoProxyConfig;
+import spring3.spring3study.config.proxy.v6_aop.AopConfig;
 import spring3.spring3study.trace.logtrace.LogTrace;
 import spring3.spring3study.trace.logtrace.ThreadLocalLogTrace;
 
@@ -24,7 +25,8 @@ import spring3.spring3study.trace.logtrace.ThreadLocalLogTrace;
 //@Import(value = {DynamicProxyFilterConfig.class})
 //@Import(value = {ProxyFactoryConfigV1.class, ProxyFactoryConfigV2.class})
 //@Import(value = {BeanPostProcessorConfig.class})
-@Import(value = {AutoProxyConfig.class})
+//@Import(value = {AutoProxyConfig.class})
+@Import(value = {AopConfig.class})
 @SpringBootApplication(scanBasePackages = "spring3.spring3study.app.proxy")
 public class Spring3StudyApplication {
 

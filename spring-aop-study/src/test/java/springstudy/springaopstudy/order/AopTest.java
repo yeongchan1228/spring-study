48eb6e24edcc5c097e33f29b222930922be76378
최영찬 <gks54410@gmail.com>
2022-06-03 23:49:07 +0900

    Test: AspectV3 테스트

--- spring-aop-study/src/test/java/springstudy/springaopstudy/order/AopTest.java
+++ spring-aop-study/src/test/java/springstudy/springaopstudy/order/AopTest.java
@@ -8,12 +8,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import springstudy.springaopstudy.order.aop.AspectV1;
 import springstudy.springaopstudy.order.aop.AspectV2;
+import springstudy.springaopstudy.order.aop.AspectV3;
 
 import static org.assertj.core.api.Assertions.*;
 
 @Slf4j
 @SpringBootTest
-@Import(value = {AspectV2.class})
+@Import(value = {AspectV3.class})
 public class AopTest {
     
     @Autowired OrderRepository orderRepository;

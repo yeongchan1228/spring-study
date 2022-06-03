3f8c6930f2d547c74df4e2a544583ab6e650bc35
최영찬 <gks54410@gmail.com>
2022-06-03 22:50:52 +0900

    Feat: AspectV1 추가(Pointcut 분리 X)

---
+++ spring-aop-study/src/main/java/springstudy/springaopstudy/order/aop/AspectV1.java
@@ -0,0 +1,18 @@
+package springstudy.springaopstudy.order.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+
+@Slf4j
+@Aspect
+public class AspectV1 {
+
+    @Around("execution(* springstudy.springaopstudy.order..*(..))")
+    public Object execute(ProceedingJoinPoint joinPoint) throws Throwable{
+        log.info("[log] {}", joinPoint.getSignature());
+
+        return joinPoint.proceed();
+    }
+}

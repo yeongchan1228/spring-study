cfe03eb26c9f6243d922f096f1e6c32870b26a10
최영찬 <gks54410@gmail.com>
2022-06-03 19:46:42 +0900

    Feat: AspectJ를 사용한 Pointcut 생성 및 적용

--- spring3-study/src/main/java/spring3/spring3study/config/proxy/v5_autoproxy/AutoProxyConfig.java
+++ spring3-study/src/main/java/spring3/spring3study/config/proxy/v5_autoproxy/AutoProxyConfig.java
@@ -1,6 +1,7 @@
 package spring3.spring3study.config.proxy.v5_autoproxy;
 
 import org.springframework.aop.Advisor;
+import org.springframework.aop.aspectj.AspectJExpressionPointcut;
 import org.springframework.aop.support.DefaultPointcutAdvisor;
 import org.springframework.aop.support.NameMatchMethodPointcut;
 import org.springframework.context.annotation.Bean;
@@ -15,10 +16,33 @@ import spring3.spring3study.trace.logtrace.LogTrace;
 @Import({AppV1Config.class, AppV2Config.class})
 public class AutoProxyConfig {
 
-    @Bean
+//    @Bean
     public Advisor advisor1(LogTrace logTrace) {
         NameMatchMethodPointcut nameMatchMethodPointcut = new NameMatchMethodPointcut();
         nameMatchMethodPointcut.setMappedNames("request*", "save*", "order*");
         return new DefaultPointcutAdvisor(nameMatchMethodPointcut, new LogTraceAdvice(logTrace));
+    }
+
+    /**
+     * 패키지 중심 Pointcut 적용
+     */
+//    @Bean
+    public Advisor advisor2(LogTrace logTrace) {
+        AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
+        pointcut.setExpression("execution(* spring3.spring3study.app..*(..))");
+
+        return new DefaultPointcutAdvisor(pointcut, new LogTraceAdvice(logTrace));
+    }
+
+    /**
+     * 패키지 + 메서드 이름 중심 Pointcut 적용
+     */
+    @Bean
+    public Advisor advisor3(LogTrace logTrace) {
+        AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
+        pointcut.setExpression("execution(* spring3.spring3study.app..*(..)) " +
+                "&& !execution(* spring3.spring3study.app..noLog(..))");
+
+        return new DefaultPointcutAdvisor(pointcut, new LogTraceAdvice(logTrace));
     }
 }

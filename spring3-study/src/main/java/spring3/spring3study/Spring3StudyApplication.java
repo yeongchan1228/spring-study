21b8f4635ecd2610b5150cea00db8db4301d742e
최영찬 <gks54410@gmail.com>
2022-06-03 19:07:14 +0900

    Feat: BeanPostProcessorConfig 등록

--- spring3-study/src/main/java/spring3/spring3study/Spring3StudyApplication.java
+++ spring3-study/src/main/java/spring3/spring3study/Spring3StudyApplication.java
@@ -12,6 +12,7 @@ import spring3.spring3study.config.proxy.v2_dynamicproxy.DynamicProxyBasicConfig
 import spring3.spring3study.config.proxy.v2_dynamicproxy.DynamicProxyFilterConfig;
 import spring3.spring3study.config.proxy.v3_proxyfactory.ProxyFactoryConfigV1;
 import spring3.spring3study.config.proxy.v3_proxyfactory.ProxyFactoryConfigV2;
+import spring3.spring3study.config.proxy.v4_postprocessor.BeanPostProcessorConfig;
 import spring3.spring3study.trace.logtrace.LogTrace;
 import spring3.spring3study.trace.logtrace.ThreadLocalLogTrace;
 
@@ -20,7 +21,8 @@ import spring3.spring3study.trace.logtrace.ThreadLocalLogTrace;
 //@Import({ConcreteProxyConfig.class})
 //@Import(value = {DynamicProxyBasicConfig.class})
 //@Import(value = {DynamicProxyFilterConfig.class})
-@Import(value = {ProxyFactoryConfigV1.class, ProxyFactoryConfigV2.class})
+//@Import(value = {ProxyFactoryConfigV1.class, ProxyFactoryConfigV2.class})
+@Import(value = {BeanPostProcessorConfig.class})
 @SpringBootApplication(scanBasePackages = "spring3.spring3study.app.proxy")
 public class Spring3StudyApplication {
 

738b02581cbe8fa5402f56db713c9b0e4b6b050e
최영찬 <gks54410@gmail.com>
2022-06-04 23:01:54 +0900

    Test: 내부 호출 테스트

---
+++ spring-aop-study/src/test/java/springstudy/springaopstudy/internalcall/CallServiceV0Test.java
@@ -0,0 +1,28 @@
+package springstudy.springaopstudy.internalcall;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import springstudy.springaopstudy.internalcall.aop.CallLogAspect;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@Slf4j
+@Import({CallLogAspect.class})
+@SpringBootTest
+class CallServiceV0Test {
+
+    @Autowired CallServiceV0 callServiceV0;
+
+    @Test
+    void external() {
+        callServiceV0.external();
+    }
+
+    @Test
+    void internal() {
+        callServiceV0.internal();
+    }
+}

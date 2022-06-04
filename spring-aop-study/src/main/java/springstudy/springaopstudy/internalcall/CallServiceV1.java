a8094dfc7381855a509deb7c0a557089025851b1
최영찬 <gks54410@gmail.com>
2022-06-04 23:09:51 +0900

    Feat: 순환 참조 Setter 주입으로 내부 호출 오류 해결

---
+++ spring-aop-study/src/main/java/springstudy/springaopstudy/internalcall/CallServiceV1.java
@@ -0,0 +1,32 @@
+package springstudy.springaopstudy.internalcall;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class CallServiceV1 {
+
+    private CallServiceV1 callServiceV1;
+
+    /**
+     * 생성자 주입은 생성하기 전 진행
+     * Setter 주입은 생성이 다 끝난 후 진행
+     * 2.6 이후 버전에서도 순환 참조를 아예 막음
+     * spring.main.allow-circular-references=true을 사용해야 함.
+     */
+    @Autowired
+    public void setCallServiceV1(CallServiceV1 callServiceV1) {
+        this.callServiceV1 = callServiceV1;
+    }
+
+    public void external(){
+        log.info("call external");
+        callServiceV1.internal(); // 내부 메서드 호출
+    }
+
+    public void internal(){
+        log.info("call internal");
+    }
+}

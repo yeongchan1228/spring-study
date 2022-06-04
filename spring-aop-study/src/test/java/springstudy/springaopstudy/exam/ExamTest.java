a9564ebe48eb791124de9cb191f20125d65e750a
최영찬 <gks54410@gmail.com>
2022-06-04 22:14:09 +0900

    Test: 예제 테스트

---
+++ spring-aop-study/src/test/java/springstudy/springaopstudy/exam/ExamTest.java
@@ -0,0 +1,22 @@
+package springstudy.springaopstudy.exam;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@Slf4j
+@SpringBootTest
+class ExamTest {
+
+    @Autowired ExamService examService;
+
+    @Test
+    void test(){
+        for (int i = 0; i < 5; i++) {
+            log.info("client request i = {}", i);
+            examService.request("data" + i);
+        }
+    }
+
+}

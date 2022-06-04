3ec52e926d61a76e35c5bd22ec9ca5259e39f5b6
최영찬 <gks54410@gmail.com>
2022-06-04 22:49:28 +0900

    Feat: RetryAspect 추가

---
+++ spring-aop-study/src/main/java/springstudy/springaopstudy/exam/aop/RetryAspect.java
@@ -0,0 +1,31 @@
+package springstudy.springaopstudy.exam.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import springstudy.springaopstudy.exam.annotation.Retry;
+
+@Slf4j
+@Aspect
+public class RetryAspect {
+
+    @Around("@annotation(retry)")
+    public Object retry(ProceedingJoinPoint joinPoint, Retry retry) throws Throwable {
+        log.info("[retry] {}, value = {}", joinPoint.getSignature(), retry.value());
+
+        int maxRetry = retry.value();
+        Exception exceptionHolder = null;
+
+        for (int retryCount = 1; retryCount <= maxRetry; retryCount++) {
+            try {
+                log.info("[retry] try count = {}/{}", retryCount, maxRetry);
+                return joinPoint.proceed();
+            }catch (Exception e){
+                exceptionHolder = e;
+            }
+        }
+
+        throw exceptionHolder;
+    }
+}

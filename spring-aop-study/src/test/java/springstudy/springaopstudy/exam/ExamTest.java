2adeb22369c62b75b8552d8a84d9e41de9731b1d
최영찬 <gks54410@gmail.com>
2022-06-04 22:30:41 +0900

    Test: TraceAspect 적용

--- spring-aop-study/src/test/java/springstudy/springaopstudy/exam/ExamTest.java
+++ spring-aop-study/src/test/java/springstudy/springaopstudy/exam/ExamTest.java
@@ -4,8 +4,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import springstudy.springaopstudy.exam.aop.TraceAspect;
 
 @Slf4j
+@Import({TraceAspect.class})
 @SpringBootTest
 class ExamTest {
 

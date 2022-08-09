1289a381934ddb6d967280a8aeb1026f5aebf32f
최영찬 <gks54410@gmail.com>
2022-08-09 17:24:13 +0900

    Feat: 인스턴스 생성을 위한 애노테이션 추가

--- spring-batch-study/src/main/java/springbatch/springbatchstudy/core/domain/PlainText.java
+++ spring-batch-study/src/main/java/springbatch/springbatchstudy/core/domain/PlainText.java
@@ -1,6 +1,8 @@
 package springbatch.springbatchstudy.core.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.DynamicUpdate;
 
@@ -9,6 +11,8 @@ import javax.persistence.*;
 @Entity
 @Getter
 @DynamicUpdate
+@AllArgsConstructor
+@NoArgsConstructor
 @Table(name = "plain_text")
 public class PlainText {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)

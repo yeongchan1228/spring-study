5bd0d1339c81f69ad02bbca251c365363d285fd3
최영찬 <gks54410@gmail.com>
2022-08-09 16:00:54 +0900

    Feat: BatchProcessing 활성화

--- spring-batch-study/src/main/java/springbatch/springbatchstudy/SpringBatchStudyApplication.java
+++ spring-batch-study/src/main/java/springbatch/springbatchstudy/SpringBatchStudyApplication.java
@@ -1,9 +1,11 @@
 package springbatch.springbatchstudy;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableBatchProcessing
 public class SpringBatchStudyApplication {
 
 	public static void main(String[] args) {

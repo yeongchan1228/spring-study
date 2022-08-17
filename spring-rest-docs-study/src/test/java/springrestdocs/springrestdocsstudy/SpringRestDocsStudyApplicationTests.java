2d27641e23b89a4656fa669dbba979e53d6ab8f8
최영찬 <gks54410@gmail.com>
2022-08-17 15:11:33 +0900

    Initial commit

---
+++ spring-rest-docs-study/src/test/java/springrestdocs/springrestdocsstudy/SpringRestDocsStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package springrestdocs.springrestdocsstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestDocsStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

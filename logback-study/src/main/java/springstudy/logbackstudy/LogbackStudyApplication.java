624ea186fb873908d1799d993e202e7c3c3bf1dd
ycchoi <gks54410@gmail.com>
2022-11-11 21:48:26 +0900

    Initial Commit

---
+++ logback-study/src/main/java/springstudy/logbackstudy/LogbackStudyApplication.java
@@ -0,0 +1,13 @@
+package springstudy.logbackstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogbackStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogbackStudyApplication.class, args);
+	}
+
+}
---
+++ logback-study/src/test/java/springstudy/logbackstudy/LogbackStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package springstudy.logbackstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogbackStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
